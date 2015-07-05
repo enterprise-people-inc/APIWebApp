ef8df25647be5b0cb8c44452087e2e03e5f6cdae
Balaji Ramachandran <balaji@enterprise-people.com>
2015-07-04 20:15:20 -0400

    updates v1

---
+++ app/scripts/controllers/header.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name searchPrototypeApp.controller:HeaderController
+ * @description
+ * # HeaderController
+ * Controller of the searchPrototypeApp
+ */
+angular.module('searchPrototypeApp')
+    .controller('HeaderController', ['$scope', '$location', function ($scope, $location) {
+
+	    $scope.isCollapsed = true;
+
+        $scope.isActive = function (viewLocation) {
+        	console.log(viewLocation);
+            return viewLocation === $location.path();
+        };
+
+    }]);
